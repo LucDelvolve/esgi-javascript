52b1433ec3606fb815139469069ee2c90c4161c2
LucDelvolve <31876700+LucDelvolve@users.noreply.github.com>
2019-11-26 17:09:48 +0100

    Update execice-{1}.js

--- execice-{1}.js
+++ execice-{1}.js
@@ -11,3 +11,33 @@ function capitalize(str) {
         return ucfirst(item);
     }).join(" ");
 }
+
+function camelCase(str) {
+    if(typeof str !== "string" || !str) return '';
+
+    return str.toLowerCase().split(" ").map(function(intem) {
+        return capitalize(item);
+    }).join(" ");
+}
+
+function snake_Case(str) {
+    if(typeof str !== "string" || !str) return '';
+}
+
+function leet(str) {
+    if(typeof str !== "string" || !str) return '';
+}
+
+function verlan(str) {
+    if(typeof str !== "string" || !str) return '';
+}
+
+
+function yoda(str) {
+    if(typeof str !== "string" || !str) return '';
+}
+
+
+function vig(str) {
+    if(typeof str !== "string" || !str) return '';
+}
